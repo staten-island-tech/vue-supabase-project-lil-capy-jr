5f7c31aa4ca971e10a4d39f870bc94c316e6c147
Nancyweng5 <wengalex55@gmail.com>
2026-04-27 12:30:05 -0400

    .

--- subabase-project/src/App.vue
+++ subabase-project/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './views/Home.vue'
 </script>
 
 <template>
