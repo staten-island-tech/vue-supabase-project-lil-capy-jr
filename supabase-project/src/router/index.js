01df79edfcaae5178c0ca628b11e1ea9dec44a49
Nancyweng5 <wengalex55@gmail.com>
2026-05-05 12:21:45 -0400

    .

--- supabase-project/src/router/index.js
+++ supabase-project/src/router/index.js
@@ -1,4 +1,25 @@
+import Cafe from '@/views/cafe.vue'
+import Home from '@/views/home.vue'
+import Shop from '@/views/shop.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path : "/",
+    name : "home",
+    component : Home,
+  },
+  {
+    path : "/cafe",
+    name : "cafe",
+    component : Cafe
+  },
+  {
+    path : '/shop',
+    name : 'shop',
+    component: Shop,
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
