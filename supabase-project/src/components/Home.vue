709360ef2bc80d94d11a225776e96190b8029e19
Serena <serenazhou4268@gmail.com>
2026-05-07 12:32:42 -0400

    Update Home.vue

--- supabase-project/src/components/Home.vue
+++ supabase-project/src/components/Home.vue
@@ -1,15 +1,31 @@
 <template>
-  <div>
+  <div class="website">
     <h1 class="website__title">Genshin Impact Capybara Cafe</h1>
+    <button class="play__button">Play</button>
   </div>
 </template>
 
 <script setup></script>
 
 <style scoped>
+.website {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  margin: 0;
+  background-image: url("background.jpg");
+}
+
 .website__title {
   text-align: center;
   font-family: 'Darumadrop One', sans-serif;
   font-size: 67px;
+}
+
+.play__button {
+  font-family: 'Darumadrop One', sans-serif;
+  background-color: #c63b49;
 }
 </style>
