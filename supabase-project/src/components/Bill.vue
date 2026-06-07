4b10262fc947231578619593acc9245cc37037e8
Nancyweng5 <wengalex55@gmail.com>
2026-06-06 22:19:23 -0400

    .

--- supabase-project/src/components/Bill.vue
+++ supabase-project/src/components/Bill.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class = "bill">
-    <h1></h1>
+  <div class="bill" @click="$emit('closebill')">
+
+    <h1>Day {{ day }} Complete</h1>
+
+    <h2>Earned: ${{ dailyprofit.toFixed(2) }}</h2>
+    <h2>Bills: -$20</h2>
+    <h2>
+      Net Profit:
+      ${{ (dailyprofit - 20).toFixed(2) }}
+    </h2>
+    <p>Click to continue</p>
   </div>
 </template>
-
 <script setup>
-import moneyamount from './moneyamount.vue';
+defineProps({
+  day: Number,
+  dailyProfit: Number
+})
+defineEmits(['closebill'])
 </script>
-
 <style scoped>
 </style>
